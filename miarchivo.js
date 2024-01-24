9f9a785775eaf74889922b18950af129dafbde12
Juaanx7 <juaanx7@gmail.com>
2024-01-24 19:25:29 -0300

    Se termino de implementar el DOM y storage

--- miarchivo.js
+++ miarchivo.js
@@ -1,11 +1,3 @@
-//En primer lugar se le pide al usuario que ingrese el monto a solicitar.
-//let monto = Number(prompt("Ingrese el monto a solicitar:"));
-//let monto = Number(document.getElementById("monto"));
-
-//Se pide la cantidad de cuotas en quiere devolverlo
-//let cuotas = Number(prompt("Ingrese la cantidad de cuotas en que quiera pagarlo, como maximo 12"));
-//let cuotas = Number(document.getElementById("cuotas"));
-
 let tasaInteres = calculoInteres(cuotas);
 
 let precioCuota = calculoCuotas(monto, tasaInteres, cuotas);
@@ -14,20 +6,22 @@ let devolucionTotal = devolucion(precioCuota, cuotas);
 
 let contenedor = document.getElementById("contenedor");
 
+let contenedor2 = document.getElementById("contenedor2");
+
 //Creo un objeto para almacenar valores del prestamo.
 const valores = { montoTotal: devolucionTotal, precioCuota: precioCuota };
 
 //Creo una clase con el constructor de los objetos que almacenara el array.
 class valoresDeCuotas {
-    constructor(id, valorCuota) {
+    constructor(id, precioCuota) {
         this.id = id;
-        this.valorCuota = valorCuota;
+        this.precioCuota = precioCuota;
     }
 }
 //Creo un array donde almaceno los objetos con el valor de las cuotas.
 const totalCuotas = [];
 
-//evento al dar click en el boton:
+//Evento al dar click en el boton "solicitar":
 let boton = document.getElementById("boton");
 boton.addEventListener("click", () => tomarDatos());
 
@@ -43,63 +37,76 @@ const tomarDatos = () => {
 }
 
 //En esta funcion hago todos los calculos del prestamo:
-const procesarDatos = (monto, cuotas) => {    
-    console.log("Segunda comprobacion: " + monto);
-    console.log(cuotas);
+const procesarDatos = (monto, cuotas) => {
     //Verifico que la cantidad de cuotas seleccionadas sea igual o menor a 12, en el caso de que sean mas, se notificara que supero el numero maximo de cuotas.
-    if(cuotas <= 12) {
-
+    if (cuotas <= 12) {
         for (let i = 1; i <= cuotas; i++) {
             //Con esta funcion calculo el interes del prestamo dependiendo la cantidad de cuotas que el usuario solicito.
             calculoInteres(cuotas);
-            //Atravez de este alert, se le muestra al usuario el precio de cada cuota atravez de una funcion que calcula el valor de cada una dependiendo el interes      
-            //alert("Precio cuota " + i + ": " + valores.precioCuota);
-            
+
             //Con esta funcion calculo el valor de las cuotas:
             calculoCuotas(monto, tasaInteres, cuotas);
 
             //Agrego el valor de la cuota al array.
-            totalCuotas.push(new valoresDeCuotas(i, valores.precioCuota))
+            totalCuotas.push(new valoresDeCuotas(i, valores.precioCuota,))
+
+            //Hago una verificacion de que se este almacenando el valor de cada cuota
             console.log("Precio cuota " + valores.precioCuota);
         }
+
+        //Almaceno el array de objetos en un sessionstorage:
+        sessionStorage.setItem('totalCuotas', JSON.stringify(totalCuotas));
+
+        //Recupero el array de objetos del JSON:
+        var totalCuotasRecuperado = JSON.parse(sessionStorage.getItem('totalCuotas'));
+
+        //Verifico que el array se alla recuperado correctamente:
+        console.log("Array recuperado" + totalCuotasRecuperado);
+
         //Atravez de este foreach se muestra en la pagina el numero de la cuota y su valor:
-        totalCuotas.forEach(cuota => {
+        totalCuotasRecuperado.forEach(cuota => {
             let div = document.createElement("div");
             div.innerHTML = `
-            <h2>Precio cuota: ${cuota.id} : ${valores.precioCuota}</h2>
+            <h5>Precio cuota: ${cuota.id} : $ ${valores.precioCuota}</h5>
             `;
+            contenedor2.append(div);
         });
-
 
         //Muestro en la pagina el monto total a devolver:
         let montoTotalHtml = document.getElementById("montoTotalHtml")
         montoTotalHtml.innerHTML = "En total devolvera: " + valores.montoTotal;
 
-        //Verifico que se haya cargado los valores de las cuotas al array 
-        console.log(totalCuotas.length);
-        console.log(totalCuotas[1]);
+        //Se le consulta al usuario si quiere ver el valor de una cuota en particular:
+        let consultaUsuario = document.getElementById("consultaUsuario")
+        consultaUsuario.innerHTML = "Ingrese el numero de la cuota que desea verificar: "
+        let div = document.createElement("div");
+        div.innerHTML = `
+            <button id="botonConsulta">Consultar</button>
+        `;
+        contenedor.append(div);
 
-        //Se le consulta al usuario si quiere ver el valor de una cuota en particular o finalizar el programa.
-        const respuestaUsuario = Number(prompt("Desea verificar el valor de una cuota en particular? Ingrese el numero de la cuota o en caso de que desea finalizar el programa ingrese el valor 0"));
-        console.log(respuestaUsuario);
+        //Al darle click al boton para la consulta se ejecuta lo siguiente:
+        let botonConsulta = document.getElementById("botonConsulta");
+        botonConsulta.addEventListener("click", () => {
+            //Tomo el valor que el usuario ingreso en ese input
+            let respuestaUsuario = document.getElementById("revisarCuota");
+            console.log(respuestaUsuario);
 
-        //Este es el metodo ".find" que no me funciona, el cual me serviria para buscar la cuota que el usuario desea ver.
-        const resultado = totalCuotas.find((i) => i.id === respuestaUsuario);
-        //Con este console.log verifico si se almacena algun valor en la variable "resultado", pero no lo hace.
-        console.log(resultado);
+            //Hago la busqueda en el array de la cuota que el usuario eligio:
+            let resultado = totalCuotasRecuperado.find((i) => i.id === respuestaUsuario);
+            console.log(resultado);
 
-        //Verifico que el numero ingresado por el usuario es igual o menor a la cantidad de cuotas que selecciono anteriormente. Tambien se verifica que el numero sea distinto a 0.
-        if (resultado <= cuotas && resultado != 0) {
-            let precioCuotaHtml = document.getElementById("precioCuotaHtml");
-            precioCuotaHtml.innerHTML = "Precio cuota " + respuestaUsuario + ": " + valores.precioCuota
-            //alert("Precio cuota " + respuestaUsuario + ": " + valores.precioCuota);
-        } else {
-            alert("Adios!")
-        }
-    } else {
-        alert("supero el maximo de cuotas permitido!");
+            //Verifico que el numero ingresado por el usuario es igual o menor a la cantidad de cuotas que selecciono anteriormente:
+            if (resultado <= cuotas && resultado != 0) {
+                let precioCuotaHtml = document.getElementById("precioCuotaHtml");
+                precioCuotaHtml.innerHTML = "Precio cuota $" + respuestaUsuario + ": " + valores.precioCuota
+            } else {
+                let mensajeFinal = document.getElementById("mensajeFinal");
+                mensajeFinal.innerHTML = "Verifique el numero de cuota que consulto"
+            }
+        });
     }
-    
+
 }
 
 //Calcula el interes de acuerdo las cuotas solicitadas.
